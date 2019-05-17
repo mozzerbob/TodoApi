f4f36deacf4721cce4966c4f0da3b3a8ff597924
Phil <morris.p1990@gmail.com>
2019-05-18 00:58:58 +0100

    Post add

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -49,5 +49,15 @@ namespace TodoApi.Controllers
 
             return todoItem;
         }
+
+        // POST
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
     }
 }
