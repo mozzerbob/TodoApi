0cce38c9d2f6f2f67e19729836436f291123a17c
Phil <morris.p1990@gmail.com>
2019-05-17 21:44:02 +0100

    The thing in the middle of the ententies and the database, yeah

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+/*inherited uses, may not be usefull*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
