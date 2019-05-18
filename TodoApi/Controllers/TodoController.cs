975f1e8c0b2b36e54847efd07f874cd5baf4551d
Phil <morris.p1990@gmail.com>
2019-05-18 14:59:48 +0100

    update all details for an existing item

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -59,5 +59,20 @@ namespace TodoApi.Controllers
 
             return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
         }
+
+        //PUT item x
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();  //like a null return
+        }
     }
 }
