28ec82cdb01d947b16410b133373ab620fe21696
Phil <morris.p1990@gmail.com>
2019-05-18 15:04:16 +0100

    delete item

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -74,5 +74,22 @@ namespace TodoApi.Controllers
 
             return NoContent();  //like a null return
         }
+
+        // DELETE: item x
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
