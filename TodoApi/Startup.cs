39dbe24e74a3d0f1dfb8a215aad46f9fc93c57f1
Phil <morris.p1990@gmail.com>
2019-05-27 16:38:59 +0100

    enable middleware to serve generated Swagger as JSON

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -35,6 +35,8 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
