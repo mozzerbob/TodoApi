15cc894c25179686d3fe62ca1fe5a8312b1b6325
Phil <morris.p1990@gmail.com>
2019-05-27 18:38:30 +0100

    Enable Static File Middleware

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -60,6 +60,8 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+
             app.UseSwagger();
 
             if (env.IsDevelopment())
