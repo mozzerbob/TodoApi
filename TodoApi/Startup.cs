09a1bbea4cb73d518bac03b1a8c35865efdd791b
Phil <morris.p1990@gmail.com>
2019-05-27 16:55:08 +0100

    Register the Swag generator, defining a Swagger document, to be called later

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -30,6 +30,10 @@ namespace TodoApi
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = apiName, Version = apiVersion });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
