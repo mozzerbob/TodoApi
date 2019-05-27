fb1b640de89b21fb7d6d9dcbebc68e6df3687fc7
Phil <morris.p1990@gmail.com>
2019-05-27 18:09:19 +0100

    additional context

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -53,6 +53,24 @@ namespace TodoApi.Controllers
         }
 
         // POST
+        /// <summary>
+        /// Creates a TodoItem.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "id": 1,
+        ///        "name": "Item1",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="item"></param>
+        /// <returns>A newly created TodoItem</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>   
         [HttpPost]
         public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
         {
